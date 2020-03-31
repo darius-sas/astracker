f35fcdbb600db95150384789d36874d4d3fb0406
Ana <ana.ro628@gmail.com>
2020-03-31 18:32:56 +0200

    updated the RemoteProjectFetcher to pull the latest changes

--- src/main/java/org/rug/web/helpers/RemoteProjectFetcher.java
+++ src/main/java/org/rug/web/helpers/RemoteProjectFetcher.java
@@ -2,8 +2,11 @@ package org.rug.web.helpers;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -12,29 +15,44 @@ import java.nio.file.Paths;
  */
 public class RemoteProjectFetcher {
 
+    private final static Logger logger = LoggerFactory.getLogger(RemoteProjectFetcher.class);
     private final Path destination;
 
     public RemoteProjectFetcher(Path destination){
         this.destination = destination;
     }
 
-    public Path getProjectPath(String linkOrName) {
+    /**
+     * Helper class that will take a path as an argument, and will return a Path object
+     * which holds the location of the project to be analysed.
+     *
+     * @param linkOrName
+     * @return
+     * @throws IOException
+     * @throws GitAPIException
+     */
+    public Path getProjectPath(String linkOrName) throws IOException, GitAPIException {
         if (this.isValidGitLink(linkOrName)) {
             var name = getProjectName(linkOrName);
-            var file = Paths.get(destination.toAbsolutePath().toString(), name);
-            if (!this.checkIfAlreadyCloned(file.toFile())) {
-                try {
-                    var git = Git.cloneRepository()
-                        .setURI(linkOrName)
-                        .setDirectory(file.toFile()).call();
-                    git.close();
-                } catch (GitAPIException e) {
-                    e.printStackTrace();
-                    return null;
-                }
+            var path = Paths.get(destination.toAbsolutePath().toString(), name);
+            if (!this.checkIfAlreadyCloned(path.toFile())) {
+                // The project is not present locally, clone it from the remote repository.
+                var git = Git.cloneRepository()
+                    .setURI(linkOrName)
+                    .setDirectory(path.toFile()).call();
+                git.close();
             }
-            return file;
-        }else {
+            // The project has been cloned before, update the branch to the latest version.
+            var git = Git.open(path.toFile());
+            var result = git.pull().setRemote("origin").setRemoteBranchName("master").call();
+            if (!result.isSuccessful()) {
+                logger.warn(String.format(
+                        "Was not able to pull the latest changes from the repository %s",
+                        name
+                ));
+            }
+            return path;
+        } else {
             return Paths.get(destination.toAbsolutePath().toString(), linkOrName);
         }
     }
@@ -50,6 +68,14 @@ public class RemoteProjectFetcher {
         return file.exists() && file.isDirectory() && list != null && list.length > 0;
     }
 
+    /**
+     * Will return the name of a project extracted from the path given as input.
+     * - If the path is a Git link, it will look between the last "/" and "." symbols.
+     * - If the path is just a name, it will return the same string.
+     *
+     * @param linkOrName
+     * @return
+     */
     public String getProjectName(String linkOrName){
         var slashIndex = linkOrName.lastIndexOf("/");
         var dotIndex = linkOrName.lastIndexOf(".");
