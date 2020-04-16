8787467309e0808855d8edc5a5835e7e6693866c
Ana <ana.ro628@gmail.com>
2020-04-16 11:25:41 +0200

    implemented ASTrackerWebRunnerTest

---
+++ src/test/java/org/rug/simpletests/web/ASTrackerWebRunnerTest.java
@@ -0,0 +1,54 @@
+package org.rug.simpletests.web;
+
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.rug.web.ASTrackerWebRunner;
+import org.rug.web.WebAnalysisController;
+import org.springframework.util.FileSystemUtils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ASTrackerWebRunnerTest {
+    Map<String,String> requestParameters;
+    ASTrackerWebRunner asTrackerWebRunner;
+
+    @Test
+    void testAStrackerWebRunner() throws IOException, GitAPIException {
+        assertTrue(Files.exists(Paths.get("./output-folder")));
+        assertTrue(Files.exists(Paths.get("./output-folder/trackASOutput")));
+        assertTrue(Files.exists(Paths.get("./output-folder/arcanOutput")));
+        assertTrue(Files.exists(Paths.get("./cloned-projects")));
+        assertTrue(Files.exists(Paths.get("./states")));
+    }
+
+    @Test
+    void testWebRunnerPyne() throws Exception {
+        this.requestParameters = new HashMap<>();
+        this.requestParameters.put("project", "https://github.com/darius-sas/pyne.git");
+        this.requestParameters.put("language", "java");
+        this.asTrackerWebRunner = new ASTrackerWebRunner(this.requestParameters);
+
+        FileSystemUtils.deleteRecursively(Paths.get("./states/pyne").toFile());
+
+        assertEquals(WebAnalysisController.Result.SUCCESS, asTrackerWebRunner.run());
+        assertTrue(Files.exists(Paths.get("./states/pyne")));
+        assertTrue(Files.exists(Paths.get("./cloned-projects/pyne")));
+        assertTrue(Files.exists(Paths.get("./output-folder/arcanOutput/pyne")));
+        assertTrue(Files.exists(Paths.get("./output-folder/trackASOutput/pyne")));
+        assertEquals("pyne", this.asTrackerWebRunner.getProjectName());
+
+        assertEquals(WebAnalysisController.Result.SKIPPED, asTrackerWebRunner.run());
+        assertTrue(
+                FileSystemUtils.deleteRecursively(Paths.get("./states/pyne").toFile()),
+                "The folder was not deleted after performing the analysis."
+        );
+    }
+}
